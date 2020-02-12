e5b44c9c10a3d98fbe36e5e8d48f5079ed4c5bf8
tonythick <thetonyvan@gmail.com>
2020-02-12 14:29:26 -0600

    app bar on other pages

--- trainer_app_demo/lib/src/wikipage.dart
+++ trainer_app_demo/lib/src/wikipage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:getflutter/getflutter.dart';
 
 class WikiPage extends StatefulWidget {
   WikiPage({Key key, this.title}) : super(key: key);
@@ -22,8 +23,90 @@ class _WikiPageState extends State<WikiPage> {
     );
   }
 
+  Widget _appBar() {
+    return GFAppBar(
+    backgroundColor: Color(0xff575366),
+    title: Text('Wiki'),
+    centerTitle: true,
+    actions: <Widget>[
+      GFIconButton(
+        icon: Icon(Icons.filter_center_focus),
+        color: Color(0xffffffff),
+        onPressed: null,
+      )
+    ],
+  );
+ }
+
+ Widget _drawer() {
+  return GFDrawer(
+    color: Color(0xff575366),
+    elevation: 3,
+    child: ListView(
+      padding: EdgeInsets.zero,
+      children: <Widget>[
+        GFDrawerHeader(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Color(0xff847f94), Color(0xff575366)]
+            )),
+          currentAccountPicture: GFAvatar(
+            radius: 100.0,
+            backgroundImage: NetworkImage("https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"),  
+          ),
+          child: Column(
+             mainAxisAlignment: MainAxisAlignment.center,
+             crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text('John D'),
+              Text('johnd@email.com')
+            ],
+          )
+        ),
+        Card(
+            color: Color(0xff9eacdd),
+            child: ListTile(
+            leading: Icon(Icons.account_circle),
+            title: Text("Profile"),
+            onTap: null
+          ),
+        ),
+        Card(
+            color: Color(0xff9eacdd),
+            child: ListTile(
+            leading: Icon(Icons.image),
+            title: Text("Photos"),
+            onTap: null
+          ),
+        ),
+        Card(
+            color: Color(0xff9eacdd),
+            child: ListTile(
+            leading: Icon(Icons.timeline),
+            title: Text("Report"),
+            onTap: null
+          ),
+        ),
+          Card(
+            color: Color(0xff9eacdd),
+            child: ListTile(
+            leading: Icon(Icons.perm_data_setting),
+            title: Text("Settings"),
+            onTap: null
+          ),
+        )
+      ]
+    ),
+
+  );
+}
+
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: _appBar(),
+      drawer: _drawer(),
       body: Stack(
         children: <Widget>[
           Container(
